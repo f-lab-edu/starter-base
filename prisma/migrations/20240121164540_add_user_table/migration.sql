24a70be4c9bd304283e0929cecd661b17c4411dc
bom2 <boo072963@naver.com>
2024-01-22 01:47:12 +0900

    user role 컬럼 default value 추가

--- prisma/migrations/20240121164540_add_user_table/migration.sql
+++ prisma/migrations/20240121164540_add_user_table/migration.sql
@@ -1,10 +1,10 @@
 -- CreateTable
 CREATE TABLE `user` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `username` VARCHAR(191) NOT NULL,
-    `email` VARCHAR(191) NOT NULL,
-    `password` VARCHAR(191) NOT NULL,
-    `role` ENUM('USER', 'ADMIN') NOT NULL,
+    `username` VARCHAR(12) NOT NULL,
+    `email` VARCHAR(254) NOT NULL,
+    `password` VARCHAR(128) NOT NULL,
+    `role` ENUM('USER', 'ADMIN') NOT NULL DEFAULT 'USER',
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updated_at` DATETIME(3) NOT NULL,
 
