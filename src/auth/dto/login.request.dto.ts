ce3924b13be3404846bafce3c0c5d6bd92ffddf7
bom2 <boo072963@naver.com>
2024-02-03 19:51:09 +0900

    login request dto - 세부적인 decorator 제거

--- src/auth/dto/login.request.dto.ts
+++ src/auth/dto/login.request.dto.ts
@@ -1,11 +1,9 @@
-import { IsEmail, IsString, IsStrongPassword, MaxLength } from 'class-validator'
+import { IsString } from 'class-validator'
 
 export class LoginRequestDto {
-  @IsEmail()
+  @IsString()
   public email: string
 
   @IsString()
-  @MaxLength(16)
-  @IsStrongPassword({ minLength: 8, minLowercase: 1, minUppercase: 0, minNumbers: 1, minSymbols: 1 })
   public password: string
 }
