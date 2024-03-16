92ef6d1ab2004ae61f49ac0a3d59bf2d680c3af2
bom2 <boo072963@naver.com>
2024-03-17 02:46:51 +0900

    login api swagger 문서화

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,12 @@
 import { Body, Controller, Post, Res } from '@nestjs/common'
 import { Response } from 'express'
+import { ApiTags } from '@nestjs/swagger'
 
 import { ACCESS_TOKEN_EXPIRATION_TIME, REFRESH_TOKEN_EXPIRATION_TIME } from './constants'
 import { LoginRequestDto } from './dto'
 import { AuthService } from './auth.service'
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
@@ -15,7 +17,7 @@ export class AuthController {
 
     // token들 cookie로 설정
     res.cookie('access_token', access_token, {
-      secure: true,
+      secure: false, // TODO: 인증서 적용 시 true
       httpOnly: true,
       sameSite: 'strict',
       maxAge: ACCESS_TOKEN_EXPIRATION_TIME,
@@ -23,7 +25,7 @@ export class AuthController {
       path: '/',
     })
     res.cookie('refresh_token', refresh_token, {
-      secure: true,
+      secure: false, // TODO: 인증서 적용 시 true
       httpOnly: true,
       sameSite: 'strict',
       maxAge: REFRESH_TOKEN_EXPIRATION_TIME,
