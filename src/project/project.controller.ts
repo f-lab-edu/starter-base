a2a117537ee40cbcedeeaa6440d3700e99a4f413
bom2 <43135366+bomi94436@users.noreply.github.com>
2024-03-16 23:20:28 +0900

    프로젝트 리스트 조회 api (#27)
    * Project schema 업데이트
    
    * 개발환경 bcrypt 관련 에러 픽스
    
    * express req.user 타입 지정 등
    
    * nodejs 서버 타임존 설정
    
    * 프로젝트 domain, 프로젝트 생성 api 추가 등
    
    * project schema 업데이트
    
    * project domain - state, builder, state factory 추가 등
    
    * import 구문 정리
    
    * domain관련 폴더 이동
    
    * 프로젝트 생성 api - 사용자 당 최대 draft 프로젝트 생성개수 제한
    
    * 불필요한 import 제거
    
    * upsert -> create로 변경
    
    * project state factory - return 타입 정의
    
    * project 생성 api - 생성된 데이터 response 전달
    
    * project category 생성 api - 생성된 데이터 response로 전달
    
    * user 생성 api - 생성된 데이터 response로 전달
    
    * bigint 내장객체 - json serialization용 함수 추가
    
    * 전역 logger(winston) 정의, api 로깅용 interceptor 및 middleware 적용
    
    * PR template 수정
    
    * 불필요한 코드 제거
    
    * 프로젝트 리스트 조회 api 구현
    
    * AuthGuard 사용 위치 변경
    
    * 일부 로직 수정
    
    * 중복선언 제거

--- src/project/project.controller.ts
+++ src/project/project.controller.ts
@@ -1,16 +1,22 @@
-import { Body, Controller, Post, UseGuards, Req } from '@nestjs/common'
-import { CreateProjectRequestDto, CreateProjectResponseDto } from './dto'
+import { Body, Controller, Post, UseGuards, Req, Get, Query } from '@nestjs/common'
+import { CreateProjectRequestDto, CreateProjectResponseDto, GetProjectsResponseDto } from './dto'
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
 import { Request } from 'express'
 import { ProjectService } from './project.service'
+import { PageRequestDto } from 'src/pagination/dto'
 
 @Controller('project')
+@UseGuards(JwtAuthGuard)
 export class ProjectController {
   constructor(private readonly projectService: ProjectService) {}
 
   @Post()
-  @UseGuards(JwtAuthGuard)
   async createProject(@Body() dto: CreateProjectRequestDto, @Req() req: Request): Promise<CreateProjectResponseDto> {
     return await this.projectService.createProject(dto, req.user.userId)
+  }
+
+  @Get()
+  async getProjects(@Query() dto: PageRequestDto): Promise<GetProjectsResponseDto> {
+    return await this.projectService.getProjects(dto)
   }
 }
