779b887cea8ab41f1f6cb507b01524cceb613ec5
bom2 <boo072963@naver.com>
2024-03-17 03:23:53 +0900

    user api swagger 문서화

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,15 @@
 import { Body, Controller, Post } from '@nestjs/common'
+import { ApiCreatedResponse, ApiTags } from '@nestjs/swagger'
 import { UsersService } from './users.service'
 import { CreateUserRequestDto, CreateUserResponseDto } from './dto'
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @ApiCreatedResponse({ type: CreateUserRequestDto })
   async createUser(@Body() dto: CreateUserRequestDto): Promise<CreateUserResponseDto> {
     return await this.usersService.createUser(dto)
   }
