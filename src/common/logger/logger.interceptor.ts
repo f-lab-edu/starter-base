5068627045ef45d543bd256cb02d7a2ab0338bcd
bom2 <boo072963@naver.com>
2024-03-17 18:40:45 +0900

    logger interceptor log level 원복

--- src/common/logger/logger.interceptor.ts
+++ src/common/logger/logger.interceptor.ts
@@ -9,10 +9,10 @@ export class LoggerInterceptor implements NestInterceptor {
     return call$.handle().pipe(
       tap({
         next: () => {
-          Logger.log(`${method} ${url} +${Date.now() - now}ms`, context.getClass().name)
+          Logger.debug(`${method} ${url} +${Date.now() - now}ms`, context.getClass().name)
         },
         error: (error) => {
-          Logger.error(`${method} ${url} +${Date.now() - now}ms ${error}`, context.getClass().name)
+          Logger.debug(`${method} ${url} +${Date.now() - now}ms ${error}`, context.getClass().name)
         },
       }),
     )
