5d083b69a54ebfeac376febd512c25f9850e4851
bom2 <boo072963@naver.com>
2024-04-16 02:01:57 +0900

    사용자 역할 체크 Guard 정의

---
+++ src/users/roles.guard.ts
@@ -0,0 +1,22 @@
+import { CanActivate, ExecutionContext, Injectable, mixin, Type } from '@nestjs/common'
+import { UserRole } from '@prisma/client'
+import { JwtDto } from '../auth/dto'
+
+export const RolesGuard = (...roles: UserRole[]): Type<CanActivate> => {
+  @Injectable()
+  class RolesGuardMixin implements CanActivate {
+    canActivate(context: ExecutionContext): boolean {
+      if (!roles) {
+        return true
+      }
+      const request = context.switchToHttp().getRequest()
+      const user: JwtDto = request.user
+
+      console.log(request.user)
+
+      return user && roles.includes(user.userRole)
+    }
+  }
+
+  return mixin(RolesGuardMixin)
+}
