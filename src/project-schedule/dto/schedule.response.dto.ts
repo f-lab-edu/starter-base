b0ab70c5e615179628ddac37523801eafc9de343
bom2 <boo072963@naver.com>
2024-04-19 23:33:09 +0900

    프로젝트 스케줄 조회 api 추가

--- src/project-schedule/dto/schedule.response.dto.ts
+++ src/project-schedule/dto/schedule.response.dto.ts
@@ -4,16 +4,16 @@ export class ScheduleResponseDto {
   @ApiProperty()
   id: number
 
-  @ApiProperty()
+  @ApiProperty({ example: '2022-01-20T00:00:00.000Z' })
   funding_start_date: Date
 
-  @ApiProperty()
+  @ApiProperty({ example: '2022-01-20T00:00:00.000Z' })
   funding_due_date: Date
 
-  @ApiProperty()
+  @ApiProperty({ example: '2022-01-20T00:00:00.000Z' })
   payment_due_date: Date
 
-  @ApiProperty()
+  @ApiProperty({ example: '2022-01-20T00:00:00.000Z' })
   payment_settlement_date: Date
 
   @ApiProperty()
--- src/project-schedule/project-schedule.controller.ts
+++ src/project-schedule/project-schedule.controller.ts
@@ -1,5 +1,5 @@
-import { Body, Controller, Param, ParseIntPipe, Patch, Post } from '@nestjs/common'
-import { ApiCreatedResponse, ApiTags } from '@nestjs/swagger'
+import { Body, Controller, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common'
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger'
 import { ProjectScheduleService } from './project-schedule.service'
 import { CreateScheduleRequestDto, ScheduleResponseDto } from './dto'
 
@@ -10,6 +10,9 @@ import { CreateScheduleRequestDto, ScheduleResponseDto } from './dto'
 export class ProjectScheduleController {
   constructor(private readonly scheduleService: ProjectScheduleService) {}
 
+  /**
+   * 프로젝트 스케줄 생성
+   */
   @Post('/project/:projectId/schedule')
   @ApiCreatedResponse({ type: ScheduleResponseDto })
   async createSchedule(
@@ -19,6 +22,27 @@ export class ProjectScheduleController {
     return await this.scheduleService.createSchedule(projectId, dto)
   }
 
+  /**
+   * 프로젝트 스케줄 조회
+   */
+  @Get('/project/:projectId/schedule')
+  @ApiOkResponse({ type: ScheduleResponseDto })
+  async getScheduleByProjectId(@Param('projectId', ParseIntPipe) projectId: number): Promise<ScheduleResponseDto> {
+    return await this.scheduleService.getSchedule({ project_id: projectId })
+  }
+
+  /**
+   * 프로젝트 스케줄 단일 조회
+   */
+  @Get('/project/schedule/:scheduleId')
+  @ApiOkResponse({ type: ScheduleResponseDto })
+  async getScheduleByScheduleId(@Param('scheduleId', ParseIntPipe) scheduleId: number): Promise<ScheduleResponseDto> {
+    return await this.scheduleService.getSchedule({ id: scheduleId })
+  }
+
+  /**
+   * 프로젝트 스케줄 수정
+   */
   @Patch('/project/schedule/:scheduleId')
   async updateSchedule() {}
 }
