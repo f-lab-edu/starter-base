7f614adf8a0c7da6cf28a3e744ed3453f81b09ef
bom2 <boo072963@naver.com>
2024-05-01 20:35:00 +0900

    roles guard 재정의

--- src/users/roles.guard.ts
+++ src/users/roles.guard.ts
@@ -1,20 +1,41 @@
-import { CanActivate, ExecutionContext, Injectable, mixin, Type } from '@nestjs/common'
+import { BadRequestException, CanActivate, ExecutionContext, Injectable, SetMetadata } from '@nestjs/common'
+import { Reflector } from '@nestjs/core'
 import { UserRole } from '@prisma/client'
 import { JwtDto } from '../auth/dto'
+import { ProjectService } from '../project/project.service'
 
-export const RolesGuard = (...roles: UserRole[]): Type<CanActivate> => {
-  @Injectable()
-  class RolesGuardMixin implements CanActivate {
-    canActivate(context: ExecutionContext): boolean {
-      if (!roles) {
-        return true
-      }
-      const request = context.switchToHttp().getRequest()
-      const user: JwtDto = request.user
+const ROLES_KEY = 'roles' as const
+export const Roles = (roles: (UserRole | 'CREATOR')[]) => SetMetadata(ROLES_KEY, roles)
 
-      return user && roles.includes(user.userRole)
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector, private readonly projectService: ProjectService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const roles: Parameters<typeof Roles>[0] = this.reflector.getAllAndOverride(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ])
+
+    if (!roles?.length) {
+      return true
     }
-  }
 
-  return mixin(RolesGuardMixin)
+    const request = context.switchToHttp().getRequest<{ params: { projectId: string }; user: JwtDto }>()
+    const user = request.user
+
+    if (!user) {
+      throw new BadRequestException('Access is available after login')
+    }
+
+    if (!roles.includes('CREATOR')) {
+      return roles.includes(user.userRole)
+    }
+
+    const projectId = Number(request.params.projectId)
+    const isCreator = await this.projectService.checkIsCreator({ projectId, userId: user.userId })
+
+    if (!roles.filter((r) => r !== 'CREATOR').length) return isCreator
+    return isCreator && roles.filter((r) => r !== 'CREATOR').includes(user.userRole)
+  }
 }
